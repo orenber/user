ca85542be62fb0b156c87a2caeaf718a42b06372
oren <orenber@hotmail.com>
2015-12-28 08:14:23 +0200

    update file cs

---
+++ desktop/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Budsoft();
+            Console.ReadKey();
+
+           
+        }
+
+
+
+       static void Budsoft()
+        {
+
+            for (int i = 1; i <= 100; i++)
+            {
+
+
+                if (i % 15 == 0)
+                { 
+                
+                   Console.WriteLine("BudSoft");
+                
+                
+                
+                }
+                else if (i % 5 == 0)
+
+                {
+
+                    Console.WriteLine("Soft");
+                
+                }
+                else if (i % 3 == 0)
+                {
+
+
+                    Console.WriteLine("Bud");
+
+
+                }
+
+                else
+                {
+
+                    Console.WriteLine(i.ToString());
+
+
+
+                
+                
+                
+                
+                }
+            
+            
+            
+            
+            
+            }
+         
+        
+
+        
+        
+        
+        }
+
+
+
+
+    }
+}
